8cb2c610b3db9aa2995a19bd5284cb577823e44e
kalilBelgoumri <kalil.belgoumri@gmail.com>
2022-10-11 22:12:28 +0200

    add react-router-dom link

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,16 +6,19 @@ import Landing from "../Landing";
 import Login from "../Login";
 import Signup from "../Signup";
 import Welcome from "../Welcome";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Header />
-      <Login />
-      <ErrorPage />
-      <Signup />
-      <Welcome />
       <Landing />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<ErrorPage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Welcome />} />
+      </Routes>
       <Footer />
     </div>
   );
